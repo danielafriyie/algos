8d70a0599a4af57327942a5206e86f29437ae644
danielafriyie <afriyiedaniel1@outlook.com>
2023-05-21 17:55:07 +0000

    did some refactoring

--- search_and_sort/python/bubble_sort.py
+++ search_and_sort/python/bubble_sort.py
@@ -18,3 +18,6 @@ if __name__ == "__main__":
     lst = [1, 3, 1, 5, 3, 6, 9, 10, 2, 7, 99, 32, 5, 2, 676, 98, 0, 456, 0, 234, 12, 567, 6, 34, 667, 2]
     bubble_sort(lst)
     print(lst)
+    lst2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    bubble_sort(lst2)
+    print(lst2)
