472b707de32d0bb7c6342b22328bc683076fced6
danielafriyie <afriyiedaniel1@outlook.com>
2023-07-14 20:00:03 +0000

    did some refactoring

--- api_wrapper/thecatapi/python/src/adapter.py
+++ api_wrapper/thecatapi/python/src/adapter.py
@@ -1,10 +1,8 @@
-import json
 import typing
 import logging
 
 import requests as rq
 
-import models
 import exceptions
 
 
@@ -25,27 +23,23 @@ class Adapter:
         self._session = rq.Session()
         self._session.verify = self._ssl_verify
         self._session.headers.update({
-            "x-api-key": self._api_key
+            "x-api-key": self._api_key,
+            "Content-Type": "application/json"
         })
 
         if not ssl_verify:
             self._logger.warning("SSL verification disabled! Enabling it is strongly advised!")
             rq.packages.urllib3.disable_warnings()
 
-    @staticmethod
-    def to_json(response: rq.Response) -> typing.Union[list[dict], dict, None]:
-        try:
-            return response.json()
-        except json.JSONDecodeError:
-            return None
-
     def request(self,
                 method: typing.Literal["GET", "POST", "DELETE"],
                 endpoint: str,
                 check_status: typing.Optional[bool] = True,
                 data: typing.Optional[dict] = None,
-                params: typing.Optional[dict] = None) -> models.Model:
+                params: typing.Optional[dict] = None) -> rq.Response:
         url = self._BASE_URL + endpoint
+        self._logger.debug(f"Making request, method: {method}, url: {url}")
+
         try:
             response = self._session.request(method, url, params=params, data=data)
         except rq.exceptions.RequestException as e:
@@ -58,32 +52,20 @@ class Adapter:
                 self._logger.warning(f"Status code: {status_code}, reason: {response.reason}")
                 raise exceptions.TheCatAPIException(response.text)
 
-        try:
-            body = response.json()
-        except (json.JSONDecodeError, ValueError) as e:
-            self._logger.exception(e)
-            raise exceptions.TheCatAPIException("Bad JSON in response!")
-
         self._logger.debug(f"SUCCESS, status code: {status_code}, reason: {response.reason}")
-        return models.Model(status_code, "", body)
+        return response
 
-    def get(self, endpoint: str, **params: typing.Any) -> models.Model:
+    def get(self, endpoint: str, **params: typing.Any) -> rq.Response:
         return self.request("GET", endpoint, params=params)
 
     def post(self,
              endpoint: str,
              data: typing.Optional[dict] = None,
-             params: typing.Optional[dict] = None) -> models.Model:
+             params: typing.Optional[dict] = None) -> rq.Response:
         return self.request("POST", endpoint, data=data, params=params)
 
     def delete(self,
                endpoint: str,
                data: typing.Optional[dict] = None,
-               params: typing.Optional[dict] = None) -> models.Model:
+               params: typing.Optional[dict] = None) -> rq.Response:
         return self.request("DELETE", endpoint, data=data, params=params)
-
-
-if __name__ == "__main__":
-    a = Adapter("api.thecatapi.com")
-    r = a.get("images/search", limit=1)
-    print(r)
