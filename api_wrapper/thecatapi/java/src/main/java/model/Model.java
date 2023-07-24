7abf9df826cfec82ef2ddadf39578cd7f3a40930
danielafriyie <afriyiedaniel1@outlook.com>
2023-07-24 16:39:55 +0000

    working on models

---
+++ api_wrapper/thecatapi/java/src/main/java/model/Model.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Model {
+    private final int id;
+
+    public Model(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
