cd4640eabf8668041f1dffc34cf022977c0c5777
danielafriyie <afriyiedaniel1@outlook.com>
2023-07-24 15:34:50 +0000

    did some refactoring

--- api_wrapper/thecatapi/java/src/main/java/core/Adapter.java
+++ api_wrapper/thecatapi/java/src/main/java/core/Adapter.java
@@ -8,8 +8,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.HttpUrl;
-import okhttp3.Call;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import com.google.gson.Gson;
