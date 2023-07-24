61b9813c75ef8318db07b086fe41f18301f77ec2
danielafriyie <afriyiedaniel1@outlook.com>
2023-07-24 14:01:40 +0000

    added new project

---
+++ api_wrapper/thecatapi/java/src/main/java/core/Adapter.java
@@ -0,0 +1,15 @@
+package core;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.HttpUrl;
+import okhttp3.Call;
+
+public class Adapter {
+
+
+}
