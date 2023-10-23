595d6bdc1bd408336627d5efccb080d63bcd8b40
danielafriyie <afriyiedaniel1@outlook.com>
2023-10-23 14:42:05 +0000

    im -c -java BinaryTree

--- BinaryTree/java/src/BinaryTree.java
+++ BinaryTree/java/src/BinaryTree.java
@@ -1,6 +1,172 @@
+import java.util.List;
+import java.util.Arrays;
 import java.util.ArrayList;
 
 public class BinaryTree<E> {
+    private Node<E> root;
+
+    public BinaryTree(Node<E> root) {
+        this.root = root;
+        this.root.setTree(this);
+    }
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public Node<E> getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node<E> node) {
+        this.root = node;
+    }
+
+    private void validate(Node<E> node) {
+        if (node.getTree() != this)
+            throw new IllegalArgumentException("Node does not belong to this tree!");
+    }
+
+    public Node<E> addLeft(Node<E> parent, E element) {
+        validate(parent);
+        Node<E> node = new Node<>(element);
+        node.setTree(this);
+        node.setParent(parent);
+        parent.setLeft(node);
+        return node;
+    }
+
+    public Node<E> addRight(Node<E> parent, E element) {
+        validate(parent);
+        Node<E> node = new Node<>(element);
+        node.setTree(this);
+        node.setParent(parent);
+        parent.setRight(node);
+        return node;
+    }
+
+    public boolean isRoot(Node<E> node) {
+        return node == this.root;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public static <T> boolean isLeaf(Node<T> node) {
+        return !node.hasChildren();
+    }
+
+    @SuppressWarnings("all")
+    public List<List<Node<E>>> getLayers() {
+        if (isEmpty())
+            return new ArrayList<>();
+        List<List<Node<E>>> output = new ArrayList<>();
+        output.add(Arrays.asList(root));
+        if (isLeaf(root)) {
+            return output;
+        }
+        List<Node<E>> children = root.children();
+        while (!children.isEmpty()) {
+            output.add(children);
+            List<Node<E>> temp = new ArrayList<>();
+            for (Node<E> child : children) {
+                temp.addAll(child.children());
+            }
+            children = temp;
+        }
+        return output;
+    }
+
+    public int depth() {
+        return getLayers().size();
+    }
+
+    public int height() {
+        List<List<Node<E>>> layers = getLayers();
+        int h = 0;
+        for (List<Node<E>> list : layers) {
+            h += list.size();
+        }
+        return h;
+    }
+
+    public int size() {
+        return height();
+    }
+
+    public void visualize(Node<E> node, int level) {
+        if (isEmpty() || node == null)
+            return;
+        if (node.getRight() != null)
+            visualize(node.getRight(), level + 2);
+        System.out.println(" ".repeat(4).repeat(level) + "-> " + node.getElement());
+        if (node.getLeft() != null)
+            visualize(node.getLeft(), level + 2);
+    }
+
+    public void visualize() {
+        visualize(root, 0);
+    }
+
+    public List<Node<E>> inOrderTraversal(Node<E> node) {
+        List<Node<E>> output = new ArrayList<>();
+        if (isEmpty() || node == null)
+            return output;
+        if (node.getLeft() != null) {
+            output.addAll(inOrderTraversal(node.getLeft()));
+        }
+        output.add(node);
+        if (node.getRight() != null) {
+            output.addAll(inOrderTraversal(node.getRight()));
+        }
+        return output;
+    }
+
+    public List<Node<E>> inOrderTraversal() {
+        return inOrderTraversal(root);
+    }
+
+    public List<Node<E>> preOrderTraversal(Node<E> node) {
+        List<Node<E>> output = new ArrayList<>();
+        if (isEmpty() || node == null)
+            return output;
+        output.add(node);
+        List<Node<E>> children = node.children();
+        for (Node<E> child : children) {
+            output.addAll(preOrderTraversal(child));
+        }
+        return output;
+    }
+
+    public List<Node<E>> preOrderTraversal() {
+        return preOrderTraversal(root);
+    }
+
+    public List<Node<E>> postOrderTraversal(Node<E> node) {
+        List<Node<E>> output = new ArrayList<>();
+        if (isEmpty() || node == null)
+            return output;
+        if (node.getLeft() != null)
+            output.addAll(postOrderTraversal(node.getLeft()));
+        if (node.getRight() != null)
+            output.addAll(postOrderTraversal(node.getRight()));
+        output.add(node);
+        return output;
+    }
+
+    public List<Node<E>> postOrderTraversal() {
+        return postOrderTraversal(root);
+    }
+
+    public List<Node<E>> breathFirstTraversal() {
+        List<List<Node<E>>> layers = getLayers();
+        List<Node<E>>  output = new ArrayList<>();
+        for (List<Node<E>> list : layers) {
+            output.addAll(list);
+        }
+        return output;
+    }
 
     public static class Node<E> {
         private E element;
@@ -63,8 +229,8 @@ public class BinaryTree<E> {
             this.right = right;
         }
 
-        public ArrayList<Node<E>> children() {
-            ArrayList<Node<E>> list = new ArrayList<>();
+        public List<Node<E>> children() {
+            List<Node<E>> list = new ArrayList<>();
             if (left != null) {
                 list.add(left);
                 if (right != null) {
@@ -81,5 +247,28 @@ public class BinaryTree<E> {
         public String toString() {
             return String.format("%s(%s)", getClass().getName(), element);
         }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree<Integer> tree = new BinaryTree<>(new Node<>(0));
+        tree.addLeft(tree.root, 1);
+        tree.addRight(tree.root, 2);
+
+        tree.addLeft(tree.root.getLeft(), 3);
+        tree.addRight(tree.root.getLeft(), 4);
+
+        tree.addLeft(tree.root.getRight(), 5);
+        tree.addRight(tree.root.getRight(), 6);
+
+        tree.addLeft(tree.root.getLeft().getLeft(), 7);
+
+        System.out.println(tree.depth());
+        System.out.println(tree.height());
+        System.out.println(tree.inOrderTraversal());
+        System.out.println(tree.preOrderTraversal());
+        System.out.println(tree.postOrderTraversal());
+        System.out.println(tree.breathFirstTraversal());
+        System.out.println();
+        tree.visualize();
     }
 }
