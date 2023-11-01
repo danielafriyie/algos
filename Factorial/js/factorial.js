0b6a37d76869b01720af86c08e34f62b7ec814e5
danielafriyie <afriyiedaniel1@outlook.com>
2023-11-01 19:47:19 +0000

    im -f -js factorial

---
+++ Factorial/js/factorial.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Integer} n 
+ * @returns {Integer}
+ */
+function factorial(n) {
+    if (n < 0)
+        throw new Error("Expects value greater than zero!");
+    else if (n <= 1)
+        return 1;
+    else if (n === 2)
+        return 2;
+    return n * factorial(n - 1);
+}
+
+
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(5));
+console.log(factorial(10));
