6f93c209aac4e901654f20f2d821b4d3a0f8a19f
danielafriyie <afriyiedaniel1@outlook.com>
2024-04-11 19:43:54 +0000

    im -c -c BinaryTree

--- BinaryTree/c/binary_tree.c
+++ BinaryTree/c/binary_tree.c
@@ -59,18 +59,23 @@ int Node_height(Node *node) {
 }
 
 Node **Node_children(Node *node) {
-    Node **children = malloc(NODE_SIZE * 2);
-    if (node->left != NULL) {
-        children[0] = node->left;
-        if (node->right != NULL) {
-            children[1] = node->right;
-        }
+    if (node->left == NULL)
+        return NULL;
+
+    Node **children = calloc(2, NODE_SIZE);
+    children[0] = node->left;
+    if (node->right != NULL) {
+        children[1] = node->right;
     }
+
     return children;
 }
 
 void Node_print(Node *node) {
-    int value = *((int *) node->element);
+    if (node == NULL)
+        return;
+
+    int value = *((int *)node->element);
     printf("Node(%d)\n", value);
 }
 
@@ -102,30 +107,195 @@ void BinaryTree_set_root(BinaryTree *tree, Node *node) {
     tree->root = node;
 }
 
+Node *BinaryTree_add_left(BinaryTree *tree, Node *parent, void *element) {
+    Node *node = Node_new(element);
+    if (tree->root == NULL) {
+        BinaryTree_set_root(tree, node);
+        tree->size++;
+        return node;
+    }
+    Node_set_left(parent, node);
+    tree->size++;
+    return node;
+}
+
+Node *BinaryTree_add_right(BinaryTree *tree, Node *parent, void *element) {
+    Node *node = Node_new(element);
+    if (tree->root == NULL) {
+        BinaryTree_set_root(tree, node);
+        tree->size++;
+        return node;
+    }
+    Node_set_right(parent, node);
+    tree->size++;
+    return node;
+}
+
+void BinaryTree_preorder_traversal(Node *node) {
+    if (node == NULL)
+        return;
+
+    Node_print(node);
+
+    if (node->left != NULL) {
+        BinaryTree_preorder_traversal(node->left);
+    }
+
+    if (node->right != NULL) {
+        BinaryTree_preorder_traversal(node->right);
+    }
+}
+
+void BinaryTree_postorder_traversal(Node *node) {
+    if (node == NULL)
+        return;
+
+    if (!Node_has_children(node)) {
+        Node_print(node);
+        return;
+    }
+
+    if (node->left != NULL) {
+        BinaryTree_postorder_traversal(node->left);
+    }
+
+    if (node->right != NULL) {
+        BinaryTree_postorder_traversal(node->right);
+    }
+
+    Node_print(node);
+}
+
+void BinaryTree_inorder_traversal(Node *node) {
+    if (node == NULL) {
+        return;
+    }
+
+    if (node->left != NULL) {
+        BinaryTree_postorder_traversal(node->left);
+    }
+
+    Node_print(node);
+
+    if (node->right != NULL) {
+        BinaryTree_postorder_traversal(node->right);
+    }
+}
+
+void BinaryTree_breath_first_traversal(Node *node, bool print_node, bool continue_traversal) {
+    if (node == NULL)
+        return;
+
+    Node_print(node);
+    if (!Node_has_children(node))
+        return;
+
+    Node **children = Node_children(node);
+    int depth_size = 4, current_size = 2;
+    while (true) {
+        int counter = 0;
+        Node **temp_array = calloc(depth_size, NODE_SIZE);
+        for (int i = 0; i < current_size; i++) {
+            Node *child = children[i];
+            if (child != NULL) {
+                Node_print(child);
+                Node **childs_children = Node_children(child);
+                if (childs_children != NULL) {
+                    for (int j = 0; j < 2; j++) {
+                        Node *childs_child = childs_children[j];
+                        if (childs_child != NULL) {
+                            temp_array[counter] = childs_child;
+                            counter++;
+                        }
+                    }
+                }
+            }
+        }
+
+        free(children);
+        if (counter == 0) {
+            free(temp_array);
+            break;
+        }
+
+        children = temp_array;
+        current_size = depth_size;
+        depth_size *= 2;
+    }
+}
+
 void BinaryTree_visualize_left_to_right(BinaryTree *tree) {
     if (tree->root == NULL)
         return;
-    
-    void _vis(Node *node, int level) {
+
+    void _vis(Node * node, int level) {
         if (node->right != NULL)
-            _vis(node->right, level + 2);
-        
-        int strings = level + 4;
-        for (int i = 0; i < strings; i++) {
+            _vis(node->right, level + 4);
+
+        for (int i = 0; i < level; i++) {
             printf(" ");
         }
 
-        int value = *((int *) node->element);
+        int value = *((int *)node->element);
         printf("->%d\n", value);
 
         if (node->left != NULL)
-            _vis(node->left, level + 2);
+            _vis(node->left, level + 4);
     }
 
-    _vis(tree->root, 1);
+    _vis(tree->root, 0);
+}
+
+void cleanup(Node *node) {
+    if (node == NULL)
+        return;
+    cleanup(node->left);
+    cleanup(node->right);
+    free(node);
 }
 
 int main() {
+    int e0 = 0;
+    int e1 = 1;
+    int e2 = 2;
+    int e3 = 3;
+    int e4 = 4;
+    int e5 = 5;
+    int e6 = 6;
+    int e7 = 7;
 
+    BinaryTree *tree = BinaryTree_new();
+    BinaryTree_set_root(tree, Node_new(&e0));
+
+    BinaryTree_add_left(tree, tree->root, &e1);
+    BinaryTree_add_right(tree, tree->root, &e2);
+
+    BinaryTree_add_left(tree, tree->root->left, &e3);
+    BinaryTree_add_right(tree, tree->root->left, &e4);
+
+    BinaryTree_add_left(tree, tree->root->right, &e5);
+    BinaryTree_add_right(tree, tree->root->right, &e6);
+
+    BinaryTree_add_left(tree, tree->root->left->left, &e7);
+
+    printf("Height: %d\nSize: %d\nEmpty: %d\n", BinaryTree_height(tree), tree->size, BinaryTree_empty(tree));
+
+    printf("\nIn-order traversal\n");
+    BinaryTree_inorder_traversal(tree->root);
+
+    printf("\nPre-order traversal\n");
+    BinaryTree_preorder_traversal(tree->root);
+
+    printf("\nPost order traversal\n");
+    BinaryTree_postorder_traversal(tree->root);
+
+    printf("\nBreath first traversal\n");
+    BinaryTree_breath_first_traversal(tree->root, true, true);
+
+    printf("\nVisualize left to right\n");
+    BinaryTree_visualize_left_to_right(tree);
+
+    cleanup(tree->root);
+    free(tree);
     return 0;
 }
