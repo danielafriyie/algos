2454d76bab6b6c74d4e9c8165df27e1f304cfbda
danielafriyie <afriyiedaniel1@outlook.com>
2024-04-17 16:16:09 +0000

    im -m -c min heap priority queue

--- PriorityQueue/c/min_heap_priority_queue.c
+++ PriorityQueue/c/min_heap_priority_queue.c
@@ -97,7 +97,7 @@ Node *MinHeapBinaryTree_add_left(MinHeapBinaryTree *tree, int index, int key, vo
         return NULL;
 
     int i = (2 * index) + 1;
-    Node *node = Node_new(key, element, index);
+    Node *node = Node_new(key, element, i);
     tree->list[i] = node;
     tree->size++;
     return node;
@@ -108,7 +108,7 @@ Node *MinHeapBinaryTree_get_left(MinHeapBinaryTree *tree, int index) {
 }
 
 Node *MinHeapBinaryTree_get_node_left(MinHeapBinaryTree *tree, Node *node) {
-    return MinHeapBinaryTree_get_left(tree, node->index);
+    return (node == NULL) ? NULL : MinHeapBinaryTree_get_left(tree, node->index);
 }
 
 Node *MinHeapBinaryTree_add_right(MinHeapBinaryTree *tree, int index, int key, void *element) {
@@ -116,7 +116,7 @@ Node *MinHeapBinaryTree_add_right(MinHeapBinaryTree *tree, int index, int key, v
         return NULL;
 
     int i = (2 * index) + 2;
-    Node *node = Node_new(key, element, index);
+    Node *node = Node_new(key, element, i);
     tree->list[i] = node;
     tree->size++;
     return node;
@@ -127,7 +127,7 @@ Node *MinHeapBinaryTree_get_right(MinHeapBinaryTree *tree, int index) {
 }
 
 Node *MinHeapBinaryTree_get_node_right(MinHeapBinaryTree *tree, Node *node) {
-    return MinHeapBinaryTree_get_right(tree, node->index);
+    return (node == NULL) ? NULL : MinHeapBinaryTree_get_right(tree, node->index);
 }
 
 bool MinHeapBinaryTree_is_root(MinHeapBinaryTree *tree, Node *node) {
@@ -152,7 +152,7 @@ void MinHeapBinaryTree_swap(MinHeapBinaryTree *tree, Node *parent, Node *child) 
 void MinHeapBinaryTree_up_heap(MinHeapBinaryTree *tree, Node *node) {
     if (node == NULL)
         return;
-    
+
     Node *parent = MinHeapBinaryTree_get_node_parent(tree, node);
     if (parent == NULL)
         return;
@@ -167,7 +167,7 @@ void MinHeapBinaryTree_up_heap(MinHeapBinaryTree *tree, Node *node) {
 void MinHeapBinaryTree_down_heap(MinHeapBinaryTree *tree, Node *node) {
     if (node == NULL)
         return;
-    
+
     Node *left = MinHeapBinaryTree_get_node_left(tree, node);
     Node *right = MinHeapBinaryTree_get_node_right(tree, node);
 
@@ -227,36 +227,104 @@ int MinHeapBinaryTree_node_height(MinHeapBinaryTree *tree, Node *node) {
         return 0;
     else if (MinHeapBinaryTree_is_leaf(tree, node))
         return 1;
-    
+
     return 1 + max(MinHeapBinaryTree_node_height(tree, MinHeapBinaryTree_get_node_left(tree, node)), MinHeapBinaryTree_node_height(tree, MinHeapBinaryTree_get_node_right(tree, node)));
+}
+
+void MinHeapBinaryTree_visualize_tree(MinHeapBinaryTree *tree, Node *node, int level) {
+    Node *right = MinHeapBinaryTree_get_node_right(tree, node);
+    if (right != NULL)
+        MinHeapBinaryTree_visualize_tree(tree, right, level + 4);
+
+    for (int i = 0; i < level; i++) {
+        printf(" ");
+    }
+
+    int value = Node_get_element(node);
+    printf("->%d\n", value);
+
+    Node *left = MinHeapBinaryTree_get_node_left(tree, node);
+    if (left != NULL)
+        MinHeapBinaryTree_visualize_tree(tree, left, level + 4);
 }
 
 void MinHeapBinaryTree_visualize_left_to_right(MinHeapBinaryTree *tree) {
     if (MinHeapBinaryTree_get_root(tree) == NULL)
         return;
+        
+    MinHeapBinaryTree_visualize_tree(tree, MinHeapBinaryTree_get_root(tree), 0);
+}
 
-    void _vis(Node * node, int level) {
-        Node *right = MinHeapBinaryTree_get_node_right(tree, node);
-        if (right != NULL)
-            _vis(right, level + 4);
+typedef struct _PriorityQueue {
+    MinHeapBinaryTree *list;
+} PriorityQueue;
 
-        for (int i = 0; i < level; i++) {
-            printf(" ");
-        }
+const size_t PRIORITY_QUEUE_SIZE = sizeof(PriorityQueue);
 
-        int value = Node_get_element(node);
-        printf("->%d\n", value);
+PriorityQueue *PriorityQueue_new(int maxsize) {
+    PriorityQueue *queue = malloc(PRIORITY_QUEUE_SIZE);
+    queue->list = MinHeapBinaryTree_new(maxsize);
+    return queue;
+}
 
-        Node *left = MinHeapBinaryTree_get_node_left(tree, node);
-        if (left != NULL)
-            _vis(left, level + 4);
+int PriorityQueue_size(PriorityQueue *queue) {
+    return queue->list->size;
+}
+
+bool PriorityQueue_is_empty(PriorityQueue *queue) {
+    return MinHeapBinaryTree_is_empty(queue->list);
+}
+
+Node *PriorityQueue_insert(PriorityQueue *queue, int key, void *element) {
+    return MinHeapBinaryTree_insert(queue->list, key, element);
+}
+
+Node *PriorityQueue_min(PriorityQueue *queue) {
+    if (PriorityQueue_is_empty(queue))
+        return NULL;
+
+    return MinHeapBinaryTree_get_root(queue->list);
+}
+
+Node *PriorityQueue_remove_min(PriorityQueue *queue) {
+    return MinHeapBinaryTree_pop(queue->list);
+}
+
+void cleanup(PriorityQueue *queue) {
+    for (int i = 0; i < queue->list->size; i++) {
+        Node *node = queue->list->list[i];
+        if (node != NULL)
+            free(node);
     }
 
-    _vis(MinHeapBinaryTree_get_root(tree), 0);
+    free(queue->list->list);
+    free(queue->list);
+    free(queue);
 }
 
 int main() {
+    int e1 = 1;
+    int e2 = 2;
+    int e3 = 3;
+    int e4 = 4;
+    int e5 = 5;
+    int e6 = 6;
 
+    PriorityQueue *queue = PriorityQueue_new(6);
+    PriorityQueue_insert(queue, e5, &e5);
+    PriorityQueue_insert(queue, e3, &e3);
+    PriorityQueue_insert(queue, e2, &e2);
+    PriorityQueue_insert(queue, e6, &e6);
+    PriorityQueue_insert(queue, e4, &e4);
+    PriorityQueue_insert(queue, e1, &e1);
 
+    printf("Root: ");
+    Node_print(MinHeapBinaryTree_get_root(queue->list));
+    printf("Size: %d\nEmpty: %d\nMin node: ", PriorityQueue_size(queue), PriorityQueue_is_empty(queue));
+    Node_print(PriorityQueue_min(queue));
+    printf("\n");
+    MinHeapBinaryTree_visualize_left_to_right(queue->list);
+
+    cleanup(queue);
     return 0;
 }
