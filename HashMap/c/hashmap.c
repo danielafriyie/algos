27a1156cff574243bdebc02ea1c828ea42fde8e2
danielafriyie <afriyiedaniel1@outlook.com>
2024-04-17 17:21:05 +0000

    im -c -c LinkedList

--- HashMap/c/hashmap.c
+++ HashMap/c/hashmap.c
@@ -49,7 +49,53 @@ void Node_set_next(Node *parent, Node *child) {
     parent->next = child;
 }
 
+typedef struct _LinkedList {
+    int size;
+    Node *head;
+    Node *tail;
+} LinkedList;
 
+const size_t LINKEDLIST_SIZE = sizeof(LinkedList);
+
+LinkedList *LinkedList_new() {
+    LinkedList *list = malloc(LINKEDLIST_SIZE);
+    list->size = 0;
+    list->head = NULL;
+    list->tail = NULL;
+    return list;
+}
+
+bool LinkedList_is_empty(LinkedList *list) {
+    return list->size < 1;
+}
+
+void LinkedList_append(LinkedList *list, Node *node) {
+    if (LinkedList_is_empty(list)) {
+        list->head = node;
+        list->tail = node;
+        list->size = 1;
+        return;
+    }
+
+    Node_set_next(list->tail, node);
+    list->tail = node;
+    list->size++;
+}
+
+void LinkedList_remove(LinkedList *list, Node *node) {
+    if (node == NULL) 
+        return;
+
+    if (list->head == node) {
+        list->head = node->next;
+    } else if (list->tail == node) {
+        list->tail = NULL;
+    }
+
+    node->next = NULL;
+    list->size--;
+    free(node);
+}
 
 int main() {
 
